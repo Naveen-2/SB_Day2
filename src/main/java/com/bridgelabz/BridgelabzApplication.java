226ddc46eae488e4ac5179438275b02f36d6bc61
Naveen-2 <naveen2k15cse@gmail.com>
2022-05-18 13:15:00 +0530

    Hello message UC1

---
+++ src/main/java/com/bridgelabz/BridgelabzApplication.java
@@ -0,0 +1,14 @@
+package com.bridgelabz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BridgelabzApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello from BridgeLabz");
+		SpringApplication.run(BridgelabzApplication.class, args);
+	}
+
+}
